63869b4984afadb0b9ca957e1c0b999ca87848b7
realkinja <78376161+realkinja@users.noreply.github.com>
2026-09-14 13:39:51 +0200

    ✨ Update Music import, import CSS styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import { CountdownTimer } from "@/components/timer"
-import Music from "components/music"
+import Music from "@/components/music"
+import styles from './page.module.css'
 
 export default function Page() {
   const targetDate = new Date("2027-07-11T23:59:59")
   return (
-    <div>
+    <div className={styles.container}>
       <CountdownTimer targetDate={targetDate} />
       <Music/>
     </div>
